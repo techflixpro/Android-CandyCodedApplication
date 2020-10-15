d8cbc990c836b4f049839922be74ac5ee9f8a4ed
Sasaga Isaiah <48787896+techflixpro@users.noreply.github.com>
2020-10-15 23:11:31 +0300

    Finally Project Completed - CandyCoded Project @11:11pm

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -75,6 +75,7 @@ public class DetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        createShareIntent();
         return super.onOptionsItemSelected(item);
     }
     private void createShareIntent(){
