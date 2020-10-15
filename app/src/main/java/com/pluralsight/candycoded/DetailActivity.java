e084c2aeb71b9bbe4061ce19f66628a34464e443
Sasaga Isaiah <48787896+techflixpro@users.noreply.github.com>
2020-10-15 23:19:24 +0300

    Finally Project Completed - CandyCoded Project @11:19pm

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -81,8 +81,8 @@ public class DetailActivity extends AppCompatActivity {
     private void createShareIntent(){
         Intent shareIntent=new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
-        String shareString=SHARE_DESCRIPTION+mCandyImageUrl+HASHTAG_CANDYCODED;
-        shareIntent.putExtra("Intent.EXTRA_TEXT", shareString);
+        String shareString=SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED;
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareString);
             startActivity(shareIntent);
     }
 }
